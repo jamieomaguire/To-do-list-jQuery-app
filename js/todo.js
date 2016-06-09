d4220a86621e2591e2ba5d9d37cd5c18eb037baa
Jamie Maguire <jamieolivermaguire@googlemail.com>
2016-06-09 18:57:03 +0100

    Add files via upload

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
     $('#submit').click(function() {
+        // Take inputs value as variable
         var task = $('input[type=text]').val();
+        // Append html template to task-list
         var template = $('#taskTemplate').html();
         $('.task-list').append(template.replace("{{ task }}", task));
+        // Reset input value
         $('input[type=text]').val("");
     });
+    // Delete Task
     $(document).on('click touch', '.delete', function() {
         $(this).parents('.task').remove();
     });
+    // Toggle complete/not complete on task
     $(document).on('click touch', '.done', function() {
         $(this).parents('.task').children('.task-copy').toggleClass('completed');
     });
