faaf8ac80c38d88d9fb9ad7b7fb5ff079485ab52
Jamie Maguire <jamieolivermaguire@googlemail.com>
2016-06-09 14:31:16 +0100

    Add files via upload

--- js/todo.js
+++ js/todo.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
         $('.task-list').append(template.replace("{{ task }}", task));
         $('input[type=text]').val("");
     });
-    $(document).on('click', '.delete', function() {
+    $(document).on('click touch', '.delete', function() {
         $(this).parents('.task').remove();
     });
-    $(document).on('click', '.done', function() {
+    $(document).on('click touch', '.done', function() {
         $(this).parents('.task').children('.task-copy').toggleClass('completed');
     });
 });
